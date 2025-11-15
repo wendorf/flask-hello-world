fe421fb4a52d52954b0a17e64d75554cfd98f2f1
Dan Wendorf <dan@render.com>
2025-11-15 13:53:06 -0800

    Try uvicorn

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
+# backend/main.py
+
 from flask import Flask
 from uvicorn.middleware.wsgi import WSGIMiddleware
 
+# Your existing Flask app
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def hello_world():
     return "Hello, World!"
 
+# <-- NEW: wrap the Flask WSGI app as ASGI for uvicorn
 app = WSGIMiddleware(flask_app)
